f1ad039023b0583c65ff49b850d75051455faff1
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-02-28 12:03:07 +1300

    Add not found page.

---
+++ src/components/BrokenLink.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+const prettyPath = computed(() => {
+  return route.params.pathMatch.join('/')
+})
+</script>
+
+<template>
+  <p>The page that you've requested:</p>
+  <br />
+  <h4>{{ prettyPath }}</h4>
+  <br />
+  <p>
+    does not exist. Please use the "back" button on your browser to return to where you were, or
+    start again using the links in the menu bar.
+  </p>
+</template>
